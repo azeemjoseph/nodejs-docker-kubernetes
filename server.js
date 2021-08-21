fbc66316bdc80a70e92987657ea71f800ff47ffe
Azeem Joseph <Azeem.joseph@gmail.com>
2021-08-21 05:01:47 +0500

    server.js updated

--- server.js
+++ server.js
@@ -5,11 +5,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const woodfrog = require("./routes/wood-frog");
 
-console.log('process.env.MONGO_USER :', process.env.MONGO_USER );
-console.log('process.env.MONGO_PASSWORD :', process.env.MONGO_PASSWORD);
-console.log('process.env.MONGO_DB :', process.env.MONGO_DB);
-console.log('process.env.MONGO_CLUSTER :', process.env.MONGO_CLUSTER);
-
 const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 const path = __dirname + "/views/";
 const PORT = process.env.PORT || 8080;
@@ -29,25 +24,3 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-// router.use(function (req, res, next) {
-//   console.log("/" + req.method);
-//   next();
-// });
-
-// router.get("/", function (req, res) {
-//   res.sendFile(path + "index.html");
-// });
-
-// router.get("/wood-frog", function (req, res) {
-//   res.sendFile(path + "wood-frog.html");
-// });
-
-// app.use(express.static(path));
-// app.use("/", router);
-
-// // app.listen(PORT, function () {
-// //   console.log('Example app listening on port 8080!')
-// // })
-
-
